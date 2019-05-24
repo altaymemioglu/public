02145bde90c3746c10c88bb39d3cbd6227f86092
Altay Memioglu <Altay.Memioglu@sapiens.com>
2019-05-24 14:44:03 +0300

    Discovery Server configured to read from Config Server (localrepo for config: C:\workPersonal\cloud-config-repo   >   hoteldiscovery.properties)
    spring.application.name=hoteldiscovery
    server.port=8082
    
    eureka.instance.hostname=localhost
    
    eureka.client.serviceUrl.defaultZone=http://localhost:8082/eureka/
    eureka.client.register-with-eureka=false
    eureka.client.fetch-registry=false
    
    spring.profiles.active=development

--- reservationCloud/hotelparent/hotelconfig/src/main/java/com/hotel/cloud/config/ConfigServer.java
+++ reservationCloud/hotelparent/hotelconfig/src/main/java/com/hotel/cloud/config/ConfigServer.java
@@ -3,9 +3,11 @@ package com.hotel.cloud.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableConfigServer
+@Profile("development")
 public class ConfigServer {
 	
 	public static void main(String[] args) {
--- reservationCloud/hotelparent/hoteldiscovery/src/main/java/com/hotel/cloud/discovery/DiscoveryServer.java
+++ reservationCloud/hotelparent/hoteldiscovery/src/main/java/com/hotel/cloud/discovery/DiscoveryServer.java
@@ -3,9 +3,11 @@ package com.hotel.cloud.discovery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Profile;
 
 @EnableEurekaServer
 @SpringBootApplication
+@Profile("development")
 public class DiscoveryServer {
 
     public static void main(String[] args) {
