777e9508d7552f1e001b381e6a8740d5febd33a4
Altay Memioglu <Altay.Memioglu@sapiens.com>
2019-05-24 16:10:38 +0300

    Gateway Server integrated & Config Server is registered as an eureka client

--- reservationCloud/hotelparent/hotelconfig/src/main/java/com/hotel/cloud/config/ConfigServer.java
+++ reservationCloud/hotelparent/hotelconfig/src/main/java/com/hotel/cloud/config/ConfigServer.java
@@ -3,10 +3,12 @@ package com.hotel.cloud.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 @Profile("development")
 public class ConfigServer {
 	
